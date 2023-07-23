03a2a293c9564f669449bad8541b1798683fa58e
Jose Eneas da Silva Maria <joseeneas@me.com>
2023-07-23 18:13:28 -0400

    Fixes

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,9 @@
-//
+console.log("Execution path - 1.1 - Declare vars and consts");
 // Where to go to aquire the required data
-//
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 // declare global variables
-var a_names    = [];
+var a_names    = []; 
 var a_metadata = [];
 var a_samples  = [];
 var id         =  0;
@@ -12,15 +11,16 @@ var md         = [];
 var sp         = [];
 
 // Color gradient used in the graphs
-const colorGradient = ['rgb(250, 250, 110)', 'rgb(196, 236, 116)', 'rgb(146, 220, 126)', 
-                       'rgb(100, 201, 135)', 'rgb(057, 180, 142)', 'rgb(008, 159, 143)',
-                       'rgb(000, 137, 138)', 'rgb(008, 115, 127)', 'rgb(033, 093, 110)', 
-                       'rgb(042, 072, 088)'];
-
+const colorGradient = ['rgb(250, 250, 110)', 'rgb(196, 236, 116)', 
+                       'rgb(146, 220, 126)', 'rgb(100, 201, 135)', 
+                       'rgb(057, 180, 142)', 'rgb(008, 159, 143)',
+                       'rgb(000, 137, 138)', 'rgb(008, 115, 127)', 
+                       'rgb(033, 093, 110)', 'rgb(042, 072, 088)'];
 
 // aquire json data from the URL and prepare it to be used in the different graphs.
-d3.json(url).then(function(data) { // begin of function
+d3.json(url).then(function(data) { // begin of function retrieve data
   
+  console.log("Execution path - 2 - Retrieve from URL");
   // prepare the dropdown menu contents, Subject IDs
   for (let i=0; i < data.names.length; i++) { 
     a_names[i]    = data.names[i];
@@ -28,21 +28,29 @@ d3.json(url).then(function(data) { // begin of function
   }
 
   // prepare the metadata array
-  for (let i=0; i < data.metadata.length; i++){ a_metadata[i] = data.metadata[i] } 
+  for (let i=0; i < data.metadata.length; i++) { a_metadata[i] = data.metadata[i] } 
   
   // prepare the samples array
-  for (let i=0; i < data.samples.length;  i++){ a_samples[i]  = data.samples[i] } 
-}); // end of function
+  for (let i=0; i < data.samples.length;  i++) { a_samples[i]  = data.samples[i] } 
+}); // end of function retrieve data
 
 // filter function to get only data referent to the choosen Subject
-function selectID(m) { return m.id == id; }
+function selectID(m) { // begin of function select subject data
+  console.log("Execution path - 4 - Filter function");
+  return m.id == id; 
+}; // end of function select subject data
 
 // function to handle the Subject ID selection and process the graphs
 function PickID(md){ // begin of function
 
+  console.log("Execution path - 3.1 - Starts to process one subject");
+  console.log("Execution path - 3.2 - Prepare ID");
   // get the data related to the Subject ID choosen
   id = d3.select("#selDataset").property("value");
+  console.log("Execution path - 3.3 - Prepare Metadata");
   md = a_metadata.filter(selectID)[0];
+
+  console.log("Execution path - 3.4 - Prepare Samples");
   sp = a_samples.filter(selectID)[0];
 
   // last data cleaning action
@@ -51,6 +59,7 @@ function PickID(md){ // begin of function
   if(md.wfreq    == null) { md.wfreq    = 0};
 
   // populate the metadata table
+  console.log("Execution path - 3.5 - Prepare Metadata Table");
   d3.select("#sample-metadata-1").text(`ID        : ${md.id}`);
   d3.select("#sample-metadata-2").text(`ethnicity : ${md.ethnicity}`);
   d3.select("#sample-metadata-3").text(`gender    : ${md.gender}`);
@@ -68,11 +77,16 @@ function PickID(md){ // begin of function
   }
 
   // lets isolate the 10 top OTUs
+  console.log("Execution path - 3.6 - Filter 10 OTUs");
   let values = sp.sample_values.slice(0, 10);
   let ids    = sp.otu_ids.slice(0,10);
   let labels = sp.otu_labels.slice(0,10);
   
   // lets setup Graph1
+  // Use sample_values as the values for the bar chart.
+  // Use otu_ids       as the labels for the bar chart.
+  // Use otu_labels    as the hovertext for the chart.
+  console.log("Execution path - 3.7 - Prepare Bar");
   let barGraph = [{
     x           : values,
     y           : ids,
@@ -84,6 +98,12 @@ function PickID(md){ // begin of function
     marker      : { color : colorGradient}}];
 
   // lets setup Graph2
+  // Use otu_ids       for the x values
+  // Use sample_values for the y values
+  // Use sample_values for the marker size
+  // Use otu_ids       for the marker colors --> Instead we are using a gradient of colors for the 3 graphs
+  // Use otu_labels    for the text values
+  console.log("Execution path - 3.8 - Prepare Bubble");
   let bubbleGraph = [{
     x           : ids,
     y           : values,
@@ -94,6 +114,7 @@ function PickID(md){ // begin of function
     marker      : { color : colorGradient, size : values }}];
 
   // lets setup Graph3
+  console.log("Execution path - 3.9 - Prepare Gauge");
   let gaugeGraph = [{
       domain    : { x: [0, 1], y: [0, 1] },
       value     : md.wfreq,
@@ -103,31 +124,33 @@ function PickID(md){ // begin of function
       gauge     : {
         axis    : { range: [0, 10] },
         bar     : {'color': "darkblue"},
-        steps   : [
-          { range : [0, 1], color: 'rgb(250, 250, 110)' },
-          { range : [1, 2], color: 'rgb(196, 236, 116)' },
-          { range : [2, 3], color: 'rgb(146, 220, 126)' },
-          { range : [3, 4], color: 'rgb(100, 201, 135)' },
-          { range : [4, 5], color: 'rgb(057, 180, 142)' },
-          { range : [5, 6], color: 'rgb(008, 159, 143)' },
-          { range : [6, 7], color: 'rgb(000, 137, 138)' },
-          { range : [7, 8], color: 'rgb(008, 115, 127)' },
-          { range : [8, 9], color: 'rgb(033, 093, 110)' },
-          { range : [9,10], color: 'rgb(042, 072, 088)' }]}}];
+        steps   : [ { range : [0, 1], color: 'rgb(250, 250, 110)' },
+                    { range : [1, 2], color: 'rgb(196, 236, 116)' },
+                    { range : [2, 3], color: 'rgb(146, 220, 126)' },
+                    { range : [3, 4], color: 'rgb(100, 201, 135)' },
+                    { range : [4, 5], color: 'rgb(057, 180, 142)' },
+                    { range : [5, 6], color: 'rgb(008, 159, 143)' },
+                    { range : [6, 7], color: 'rgb(000, 137, 138)' },
+                    { range : [7, 8], color: 'rgb(008, 115, 127)' },
+                    { range : [8, 9], color: 'rgb(033, 093, 110)' },
+                    { range : [9,10], color: 'rgb(042, 072, 088)' }]}}];
 
   // adjust contents and layout for the 3 Graphs
-  let layout1 = { title: "Top 10 OTUs"              , plot_bgcolor:"Lightskyblue"};
-  let layout2 = { title: "Top 10 OTUs"              , plot_bgcolor:"Lightskyblue"};
-  let layout3 = { title: "Weekly washing frequency" , plot_bgcolor:"Lightskyblue"}; 
+
+  let layout1 = { title: "Top 10 OTUs"              , plot_bgcolor:"Lightskyblue", paper_bgcolor: "Lightskyblue"};
+
+  let layout2 = { title: "Top 10 OTUs"              , plot_bgcolor:"Lightskyblue", paper_bgcolor: "Lightskyblue"};
+  let layout3 = { title: "Weekly washing frequency" , plot_bgcolor:"Lightskyblue", paper_bgcolor: "Lightskyblue"}; 
 
   // Plot the Graphs
+  console.log("Execution path - 3.10 - Plot Bar");
   Plotly.newPlot("bar"   ,    barGraph, layout1);
+  console.log("Execution path - 3.11 - Plot Buble");
   Plotly.newPlot("bubble", bubbleGraph, layout2);
+  console.log("Execution path - 3.12 - Plot Gauge");
   Plotly.newPlot('gauge' ,  gaugeGraph, layout3);
 
 }; // end of function
-
+console.log("Execution path - 1.2 - Declare Change Handle");
 d3.selectAll("#selDataset").on("change", PickID);
-
-// let's ensure the first one is shown with a click
 d3.selectAll("#selDataset").on("click", PickID);
